34713e6447c3cca6757e2c05970b8ab4dec68d21
SebastianWeyersbach <alunotruecode+sebastian-weyersbach@gmail.com>
2023-03-30 16:13:36 -0300

    Trabalhando no selecionador do bonde

---
+++ Assets/Scripts/Batalha/TelaDoBonde.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TelaDoBonde : MonoBehaviour
+{
+    [SerializeField] Text MensagemDeTexto;
+    [SerializeField] UI_DoBonde[] EspacoMembros;
+    List<Pikomon> pikomons;
+
+    public void Inicializacao()
+    {
+        EspacoMembros = GetComponentsInChildren<UI_DoBonde>();
+    }
+
+    public void InfoBonde(List<Pikomon> pikomons)
+    {
+        this.pikomons = pikomons;
+        for (int i = 0; i < EspacoMembros.Length; i++)
+        {
+            if(i < pikomons.Count)
+            {
+                EspacoMembros[i].SetData(pikomons[i]);
+            }
+            else
+            {
+                EspacoMembros[i].gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void AtualizarBonde(int membroSelecionado)
+    {
+        for(int i = 0; i < pikomons.Count; i++)
+        {
+            if(i == membroSelecionado)
+            {
+                EspacoMembros[i].SetarSelecionado(true);
+            }
+            else
+            {
+                EspacoMembros[i].SetarSelecionado(false);
+            }
+        }
+    }
+}
