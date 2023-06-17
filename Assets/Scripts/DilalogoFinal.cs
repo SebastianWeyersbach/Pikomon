34c437e28e84dd8fa408f63e4ebc58b2f71aeee0
SebastianWeyersbach <alunotruecode+sebastian-weyersbach@gmail.com>
2023-06-17 15:43:19 -0300

    17/06(reposição)
    Mexemos no sistema de dialogo (de novo)

---
+++ Assets/Scripts/DilalogoFinal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DilalogoFinal
+{
+    [SerializeField] string nome;
+    [SerializeField][TextArea(10, 10)] List<string> falas;
+
+    public string Nome{ get{ return nome;} }
+    public List<string> Falas { get{ return falas;} }
+}
---
+++ Assets/Scripts/GerenteDilalogadorFinal.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class GerenteDlalogadorFinal : MonoBehaviour
+{
+    [SerializeField] GameObject _caixadedilalogo;
+    [SerializeField] Text textoDilalogo;
+    [SerializeField] Text NomeObj;
+    [SerializeField] int LetrasPorSegundo;
+    public Animator animFala;
+
+    public event Action DialogoEntrando;
+    public event Action DialogoSaindo;
+
+    int linhaAtual = 0;
+    DilalogoFinal dialogo;
+    bool Digitando;
+
+    public static GerenteDlalogadorFinal Instancia { get; private set;}
+
+    private void Awake()
+    {
+        Instancia = this;
+    }
+
+    void Start()
+    {
+        animFala = GameObject.FindGameObjectWithTag("GUIlherme/Dilalogo").GetComponent<Animator>();
+    }
+
+}
