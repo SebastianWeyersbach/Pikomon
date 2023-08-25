bba4ac923982ace5432fee485fabf5fda05f948b
SebastianWeyersbach <alunotruecode+sebastian-weyersbach@gmail.com>
2023-08-25 20:03:22 -0300

    Aula 25/08
    Mexemos no sistema de animação

--- Assets/Scripts/ControleNPC.cs
+++ Assets/Scripts/ControleNPC.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class ControleNPC : MonoBehaviour, Interagiveis
 {
-   [SerializeField] DilalogoFinal dilalogo;
-   public void interagir()
+    [SerializeField] DilalogoFinal dilalogo;
+    [SerializeField] List<Sprite> sprites;
+
+    AnimadorDeSprites animadorDeSprites;
+
+    private void Start()
+    {
+        animadorDeSprites = new AnimadorDeSprites(sprites,GetComponent<SpriteRenderer>());
+        animadorDeSprites.Start();
+    }
+
+    public void interagir()
     {
         Debug.Log("Interagindo com um NPC");
     }
---
+++ Assets/Scripts/Util/AnimadorDeSprites.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AnimadorDeSprites
+{
+    SpriteRenderer renderizadorDeSprites;
+    List<Sprite> frames;
+    float TaxaDeQuadros;
+
+    int FrameAtual;
+    float timer;
+    public AnimadorDeSprites(List<Sprite> frames, SpriteRenderer renderizadorDeSprites, float TaxaDeQuadros=0.30f)
+    {
+        this.frames = frames;
+        this.renderizadorDeSprites = renderizadorDeSprites;
+        this.TaxaDeQuadros = TaxaDeQuadros;
+    }
+
+    public void Start()
+    {
+        FrameAtual = 0;
+        timer = 0f;
+        renderizadorDeSprites.sprite = frames[0];
+    }
+
+    public void UpdateManual()
+    {
+        timer += Time.deltaTime;
+        if (timer > TaxaDeQuadros)
+        {
+            FrameAtual = (FrameAtual + 1) % frames.Count;
+            renderizadorDeSprites.sprite = frames[FrameAtual];
+            //timer -= TaxaDeQuadros;
+        }
+    }
+
+}
